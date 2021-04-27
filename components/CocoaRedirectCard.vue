1b8b2f3db131be59fe6f1ca4a0aa260ba165ec3d
nishidayoshikatsu <nsd24technology@gmail.com>
2021-04-28 02:21:40 +0900

    アプリボタンの位置を調整

--- components/CocoaRedirectCard.vue
+++ components/CocoaRedirectCard.vue
@@ -43,20 +43,22 @@
             </div>
           </div>
 
-          <div class="AppInstall-buttons">
+          <div class="AppInstall-block AppInstall-buttons">
             <a
               href="https://apps.apple.com/jp/app/%E6%96%B0%E5%9E%8B%E3%82%B3%E3%83%AD%E3%83%8A%E3%82%A6%E3%82%A4%E3%83%AB%E3%82%B9%E6%8E%A5%E8%A7%A6%E7%A2%BA%E8%AA%8D%E3%82%A2%E3%83%97%E3%83%AA/id1516764458?mt=8"
               class="AppStore"
+              target="_blank"
+              rel="noopener noreferrer"
             />
             <a
               href="https://play.google.com/store/apps/details?id=jp.go.mhlw.covid19radar"
               class="GooglePlay"
-            >
-              <img
+              target="_blank"
+              rel="noopener noreferrer"
+              ><img
                 alt="Google Play で手に入れよう"
                 src="https://play.google.com/intl/us-en/badges/static/images/badges/ja_badge_web_generic.png"
-              />
-            </a>
+            /></a>
           </div>
         </div>
       </v-row>
@@ -106,35 +108,29 @@ $tinySmall: 420;
   .AppInstall-buttons {
     position: relative;
     z-index: 3;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    // padding: px2vw(5) px2vw(10) px2vw(5) px2vw(25);
-    // margin-left: px2vw(16);
+    display: block;
+    padding: px2vw(5) px2vw(10) px2vw(5) px2vw(25);
+    margin-left: px2vw(16);
     font-size: px2vw(14);
     text-align: center;
-
     .AppStore {
       display: inline-block;
       width: 135px;
       height: 40px;
-      margin-right: 10px;
       overflow: hidden;
       background: url(https://linkmaker.itunes.apple.com/ja-jp/badge-lrg.svg?releaseDate=2020-06-18&kind=iossoftware&bubble=apple_music)
         no-repeat;
     }
-
     .GooglePlay {
-      //display: inline-block;
+      display: inline-block;
       width: auto;
       height: 40px;
       overflow: hidden;
-
       img {
-        width: 135px;
+        width: auto;
         height: 58px;
         object-fit: cover;
-        object-position: -9px -9px;
+        object-position: 0 -9px;
       }
     }
   }
